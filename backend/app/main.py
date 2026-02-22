7c10fadefcc799fd00d691cfd9bcadf335177f18
Drake0112 <praneethreddy0112@gmail.com>
2026-02-22 22:01:07 +0530

    addded deployed frontend link

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="CredMate API")
 # CORS Fix
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://credmatedeployment.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
