f3c244d3ab2cc3f5219b3a95d22c2f6ca276155e
Drake0112 <praneethreddy0112@gmail.com>
2026-02-22 21:51:34 +0530

    changed axios.ts

--- frontend/credmatefrontend/src/api/axios.ts
+++ frontend/credmatefrontend/src/api/axios.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:8000",
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
-API.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+
+  if (token && config.headers) {
+    config.headers.set("Authorization", `Bearer ${token}`);
   }
+
   return config;
 });
 
-export default API;
+export default api;
